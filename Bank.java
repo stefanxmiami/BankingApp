f28ab026a8888439e8f57e516570aa20ad1226d2
Stefan Ivanov <76564474+stefanxmiami@users.noreply.github.com>
2021-03-07 00:23:44 +0100

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,41 +1,22 @@
 import java.util.Scanner;
-
-
-/*
-    This is a single user intended program.
-    You can sign up and login after you sign out and your data will be saved,
-    however if another person wants to sign up, the old data still stays from the last person.
-    Also, for some reason on startup, you can't enter "sign up" or "log in" with a space in the middle.
-    I can't figure out a way for it to remove the space character.
-*/
-
-
-
-public class Bank {
+public class Bank implements GUI{
     //truth values for looping the menus
     public static boolean innerMenuLoop = true;
     public static boolean outerMenuLoop = true;
 
     public static void main(String[] args) {
-    do {
-        BankAccount bankAccount = new BankAccount();
-        SignUpLogIn sili = new SignUpLogIn();
-        Scanner sc1 = new Scanner(System.in);
+        do {
+        SignUpLogIn signUpLogIn = new SignUpLogIn();
 
-        while(sili.signUpLogIn())
-        sili.signUpLogIn();
+        //sign up and log in menu
+        while(signUpLogIn.signUpLogIn())
+                signUpLogIn.signUpLogIn();
 
         do {
-            //Print GUI
-            System.out.println("-------------------------MENU------------------------");
-            System.out.println("Enter 'Deposit'  or '+' to deposit money");
-            System.out.println("Enter 'Withdraw' or '-' to withdraw money");
-            System.out.println("Enter 'Balance'  or '=' to check balance");
-            System.out.println("Enter 'SignOut'  or '/' to sign out of your account");
-            System.out.println("Enter 'Options'  or 'o' to change username or password");
-            System.out.println("Enter 'turnoff' to shut down the system");
-            System.out.println("-------------------------MENU------------------------");
+            //Print the GUI
+            System.out.println(graphicalUserInterface);
             innerMenuLoop = true;
+
             //Select menu
             Scanner sc = new Scanner(System.in);
             String inputMenu = sc.next();
