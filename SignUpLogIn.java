8c4913dc769cb59af82f7a41be3ea32caa17824d
Stefan Ivanov <76564474+stefanxmiami@users.noreply.github.com>
2021-04-09 20:22:31 +0200

    Update SignUpLogIn.java

--- SignUpLogIn.java
+++ SignUpLogIn.java
@@ -8,52 +8,55 @@ public class SignUpLogIn extends Bank implements LogInSingUpGUI{
         BankAccount bankAccount = new BankAccount();
 
         Scanner sc = new Scanner(System.in);
-            String inputUserName = "";
-            String inputPassword = "";
+        String inputUserName = "";
+        String inputPassword = "";
 
         boolean signInLogInMenuLoop = true;
         System.out.println(LogInSingUpGUI.LSGUI);
         String s = sc.nextLine();
 
         do{
-        switch (s.toLowerCase(Locale.ROOT).trim()) {
+            switch (s.toLowerCase(Locale.ROOT).trim()) {
 
-            case "sign up":
-            case "signup":
+                case "sign up":
+                case "signup":
 
-                System.out.println("Enter Your First Name:");
-                bankAccount.setClientFirstName(sc.next());
-                System.out.println("Enter Your Last Name:");
-                bankAccount.setClientLastName(sc.next());
-                System.out.println("Enter Username:");
-                bankAccount.setUsername(sc.next());
-                System.out.println("Enter Password:");
-                bankAccount.setPassword(sc.next());
-                bankAccount.setClientAccNumber(accountNumberSetter++);
-                //savedUsers.saveUserInfo();
-                return false;
-
-            case "login":
-            case "log in":
-                System.out.println("Enter Username:");
-                inputUserName=sc.next();
-                if(savedUsers.listUserNames.contains(inputUserName)){
+                    System.out.println("Enter Your First Name:");
+                    bankAccount.setClientFirstName(sc.next());
+                    System.out.println("Enter Your Last Name:");
+                    bankAccount.setClientLastName(sc.next());
+                    System.out.println("Enter Username:");
+                    bankAccount.setUsername(sc.next());
                     System.out.println("Enter Password:");
-                    inputPassword = sc.next();
-                    if(inputPassword.equals(savedUsers.listPasswords.get(savedUsers.listUserNames.indexOf(inputUserName)))){
-                        System.out.println("Successful login!");
-                        savedUsers.parseUserInfoOnLogin(savedUsers.listPasswords.indexOf(inputPassword));
-                        return false;
-                    }else{ System.out.println("Wrong Password!"); }
-                }else{ System.out.println("Wrong Username!"); }
+                    bankAccount.setPassword(sc.next());
+                    bankAccount.setClientAccNumber(accountNumberSetter++);
+                    System.out.println("Bank account successfully created!");
+                    return false;
 
-            default:
-                System.out.println("Invalid choice!");
 
-                 signInLogInMenuLoop = false;
-                 return true;
+                case "login":
+                case "log in":
+                    System.out.println("Enter Username:");
+                    inputUserName=sc.next();
+                    if(savedUsers.listUserNames.contains(inputUserName)){
+                        System.out.println("Enter Password:");
+                        inputPassword = sc.next();
+                        if(inputPassword.equals(savedUsers.listPasswords.get(savedUsers.listUserNames.indexOf(inputUserName)))){
+                            System.out.println("Successful login!");
+                            savedUsers.parseUserInfoOnLogin(savedUsers.listPasswords.indexOf(inputPassword));
+                            return false;
+                        }else{ System.out.println("Wrong Password!"); }
+                    }else{ System.out.println("Wrong Username!");
+                        //System.out.printf("password was: "+ savedUsers.listUserNames);
+                        }
 
-        }
+                default:
+                    System.out.println("Invalid choice!");
+
+                    signInLogInMenuLoop = false;
+                    return true;
+
+            }
         }while(signInLogInMenuLoop);
     }
 }
