9de4e864afbec88b33178e72799ed6e15a3b9db6
Stefan Ivanov <76564474+stefanxmiami@users.noreply.github.com>
2021-04-09 20:20:40 +0200

    Update LogInSingUpGUI.java

--- LogInSingUpGUI.java
+++ LogInSingUpGUI.java
@@ -1,5 +1,4 @@
 public interface LogInSingUpGUI {
-    String LSGUI = "If you are a new user, you must sign up first!\n" +
-            "After a new user signs up, they must log in again.\n" +
-            "Enter 'Login' or 'SignUp'";
+    String LSGUI = "New to the BankingApp? Sign up to use all its features!\n" +
+                   "Enter 'Login' or 'SignUp'";
 }
