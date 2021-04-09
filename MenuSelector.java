242211f8d2c1dca27e98897aeeb34dcce6876777
Stefan Ivanov <76564474+stefanxmiami@users.noreply.github.com>
2021-04-09 20:21:00 +0200

    Update MenuSelector.java

--- MenuSelector.java
+++ MenuSelector.java
@@ -1,38 +1,65 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class MenuSelector extends Bank {
+
+public class MenuSelector extends Bank implements CurrencyGUI {
+    static boolean optionActive = true;
+    static boolean depositCurrencyMenuActive = true;
+    static boolean withdrawCurrencyMenuActive = true;
+
+
     public void selectMenu(String mn) {
         BankAccount bankAccount = new BankAccount();
         SavedUsers savedUsers = new SavedUsers();
         Scanner sc = new Scanner(System.in);
-        boolean optionActive = true;
-        String passVerificator = bankAccount.getPassword();
+
+        DepositSystem depositSystem = new DepositSystem();
+        WithdrawalSystem withdrawalSystem = new WithdrawalSystem();
+        BalanceSystem balanceSystem = new BalanceSystem();
+        OptionsMenu optionsMenu = new OptionsMenu();
+        CurrencyConverter currencyConverter = new CurrencyConverter();
+
+        String passVerificator = "";
 
         switch (mn.toLowerCase(Locale.ROOT).trim()) {
 
             case "deposit":
             case "+":
-                System.out.println("Enter Deposit Amount:");
-                bankAccount.depositBalance(sc.nextDouble());
+                while (depositSystem.depositSystem());
+                //depositSystem.depositSystem();
                 break;
+
             case "withdraw":
             case "-":
-                System.out.println("Enter Withdraw Amount:");
-                double temp = sc.nextDouble();
-                if(temp> bankAccount.getBalance())
-                    System.out.println("Insufficient funds!");
-                else
-                    bankAccount.withdrawBalance(temp);
+                System.out.println("You Must Verify Your Password First!");
+                passVerificator = sc.next();
+                if (passVerificator.equals(bankAccount.getPassword())) {
+                    System.out.println("Password Is Correct!");
+                    while (withdrawalSystem.withdrawalSystem());
+                } else {
+                    System.out.println("Password is incorrect!");
+                }
                 break;
+
             case "balance":
             case "=":
-                System.out.println();
-                System.out.println("-----------------------BALANCE-----------------------");
-                System.out.println("Your Balance Is:\t\t" + "$" + bankAccount.getBalance());
-                System.out.println("-----------------------BALANCE-----------------------");
-                System.out.println();
+                while (balanceSystem.balanceSystem());
+                //balanceSystem.balanceSystem();
                 break;
+
+
+            case "convert":
+            case "c":
+                System.out.println("You Must Verify Your Password First!");
+                passVerificator = sc.next();
+                if (passVerificator.equals(bankAccount.getPassword())) {
+                    System.out.println("Password Is Correct!");
+                    currencyConverter.convert();
+                } else {
+                    System.out.println("Password is incorrect!");
+                }
+                break;
+
             case "sign out":
             case "signout":
             case "/":
@@ -41,61 +68,18 @@ public class MenuSelector extends Bank {
                 savedUsers.deletePreviousUserInfo();
                 innerMenuLoop = false;
                 break;
+
             case "options":
             case "o":
-                System.out.println("User: " + bankAccount.getUsername() + " || Account Number: " + bankAccount.getClientAccNumber());
-                System.out.println("Enter 'change username' or 'cu' to change username'");
-                System.out.println("Enter 'change password' or 'cp' to change password'");
-                System.out.println("Enter 'back' or 'b' to go back to previous menu");
-                String newOptionsMenu = sc.next();
-                do {
-                    switch (newOptionsMenu.toLowerCase(Locale.ROOT).trim().replaceAll(" ", "")) {
-                        default:
-                            System.out.println("Invalid choice!");
-                            optionActive = false;
-                            break;
-                        case "back":
-                        case "b":
-                            optionActive = false;
-                            break;
-
-                        case "change username":
-                        case "changeusername":
-                        case "cu":
-                            System.out.println("You Must Verify Your Password First!");
-                            passVerificator = sc.next();
-                            if (passVerificator.equals(bankAccount.getPassword())) {
-                                System.out.println("Password Is Correct!");
-                                System.out.println("Enter A New Username:");
-                                bankAccount.setUsername(sc.next());
-                                optionActive = false;
-                                break;
-                            } else {
-                                System.out.println("Password is incorrect!");
-                            }
-                        case "change password":
-                        case "changepassword":
-                        case "cp":
-                            System.out.println("You Must Verify Your Password First!");
-                            passVerificator = sc.next();
-                            if (passVerificator.equals(bankAccount.getPassword())) {
-                                System.out.println("Password Is Correct!");
-                                System.out.println("Enter A New Username:");
-                                bankAccount.setPassword(sc.next());
-                                optionActive = false;
-                                break;
-                            } else {
-                                System.out.println("Password is incorrect!");
-                            }
-                    }}while (optionActive);
+                optionsMenu.optionsMenu();
                 break;
 
-                case "turnoff":
-                    System.out.println("Shutting down..");
-                            System.exit(0);
-                            break;
-                        default:
-                            System.out.println("You have entered a wrong value!");
-                }
+            case "turnoff":
+                System.out.println("Shutting down..");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("You have entered a wrong value!");
         }
     }
+}
