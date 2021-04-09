dd69f86ddcdaa19d9c34f09ac13592e28089bee6
Stefan Ivanov <76564474+stefanxmiami@users.noreply.github.com>
2021-04-09 20:18:20 +0200

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -6,23 +6,23 @@ public class Bank implements GUI{
 
     public static void main(String[] args) {
         do {
-        SignUpLogIn signUpLogIn = new SignUpLogIn();
+            SignUpLogIn signUpLogIn = new SignUpLogIn();
 
-        //sign up and log in menu
-        while(signUpLogIn.signUpLogIn())
-                signUpLogIn.signUpLogIn();
+            //sign up and log in menu
+            while(signUpLogIn.signUpLogIn());
 
-        do {
-            //Print the GUI
-            System.out.println(graphicalUserInterface);
-            innerMenuLoop = true;
 
-            //Select menu
-            Scanner sc = new Scanner(System.in);
-            String inputMenu = sc.next();
-            MenuSelector menuSelector = new MenuSelector();
-            menuSelector.selectMenu(inputMenu);
-        } while (innerMenuLoop);
-    }while (outerMenuLoop);
+            do {
+                //Print the GUI
+                System.out.println(graphicalUserInterface);
+                innerMenuLoop = true;
+
+                //Select menu
+                Scanner sc = new Scanner(System.in);
+                String inputMenu = sc.next();
+                MenuSelector menuSelector = new MenuSelector();
+                menuSelector.selectMenu(inputMenu);
+            } while (innerMenuLoop);
+        }while (outerMenuLoop);
     }
 }
